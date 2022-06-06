c108b57a2dbe97b4ed4758adc9630acc46275fe5
System Administrator <root@Pavels-MacBook-Pro.local>
2022-06-06 22:30:31 +0300

    First commit. Created Single View Application project in Xcode

---
+++ BookmarkManagerApp-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BookmarkManagerApp-iOS
+//
+//  Created by Pavel Palancica on 6/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
