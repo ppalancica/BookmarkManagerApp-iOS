9b425e91c5158d93ea8a2d6cb329ef3dbdd32ca4
System Administrator <root@Pavels-MacBook-Pro.local>
2022-06-07 00:05:41 +0300

    Added Core Data to the project

---
+++ BookmarkManagerApp-iOS/BookmarkListTableViewController.swift
@@ -0,0 +1,138 @@
+//
+//  BookmarkListTableViewController.swift
+//  BookmarkManagerApp-iOS
+//
+//  Created by Pavel Palancica on 6/6/22.
+//
+
+import UIKit
+import CoreData
+
+class BookmarkListTableViewController: UITableViewController {
+
+    var managedObjectContext: NSManagedObjectContext!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 44
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerViewFrame = CGRect(x: 0, y: 0, width: tableView.frame.size.width, height: 44)
+        let headerView = UIView(frame: headerViewFrame)
+        headerView.backgroundColor = .lightText
+        
+        let textField = UITextField(frame: headerViewFrame)
+        textField.placeholder = "Enter BookmarkList title"
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        textField.leftViewMode = .always
+        textField.rightView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        textField.rightViewMode = .always
+        headerView.addSubview(textField)
+        
+        return headerView
+    }
+
+}
+
+private extension BookmarkListTableViewController {
+    func initializeCoreDataStack() {
+        guard let modelURL = Bundle.main.url(forResource: "BookmarksDataModel", withExtension: "momd") else {
+            fatalError("BookmarksDataModel not found")
+        }
+        
+        guard let managedObjectModel = NSManagedObjectModel(contentsOf: modelURL) else {
+            fatalError("Unable to initialize ManagedObjectModel")
+        }
+        
+        let fileManager = FileManager()
+        guard let documentsURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            fatalError("Unable to get documents URL")
+        }
+        let storeURL = documentsURL.appendingPathComponent("BookmarksDataModel.sqlite")
+        print(storeURL)
+        
+        let persistenceStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: managedObjectModel)
+        try! persistenceStoreCoordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: storeURL, options: nil)
+        
+        let concurrencyType = NSManagedObjectContextConcurrencyType.mainQueueConcurrencyType
+        self.managedObjectContext = NSManagedObjectContext(concurrencyType: concurrencyType)
+        self.managedObjectContext.persistentStoreCoordinator = persistenceStoreCoordinator
+    }
+}
